6fca956103de0cb199d05462e4914c27f00c01f8
Igor Gorovyy <igor@kuna.family>
2024-10-21 23:53:18 +0200

    add screenshots. requests and usage try us to think about real usage and requests. add real usage and real utilization

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 ##
 ![](assets/Screenshot1.PNG)
 ##
+![](assets/Screenshot3.png)
+##
 ![](assets/Screenshot2.PNG)
--- vizualizer-test-v1.py
+++ vizualizer-test-v1.py
@@ -1,22 +1,19 @@
 import time
-import json  # Додаємо імпорт json
+import json
 import boto3
 from kubernetes import client, config
-from colorama import Fore, Style
+from colorama import Fore
 
 aws_region = 'eu-west-1'
 pricing_region = 'us-east-1'
 
 ec2_client = boto3.client('ec2', region_name=aws_region)
 
-# Завантаження конфігурації Kubernetes для конкретного контексту
 config.load_kube_config(context="arn:aws:eks:eu-west-1:294949574448:cluster/dev-1-30")
 v1 = client.CoreV1Api()
 
-# Отримуємо всі ноди
 nodes = v1.list_node().items
 
-# Функція для отримання Instance ID з анотацій ноди
 def get_instance_id(node):
     annotations = node.metadata.annotations
     if 'node.kubernetes.io/instance-id' in annotations:
@@ -24,7 +21,6 @@ def get_instance_id(node):
     else:
         return get_instance_id_by_internal_ip(node)
 
-# Функція для отримання Instance ID за InternalIP
 def get_instance_id_by_internal_ip(node):
     for addr in node.status.addresses:
         if addr.type == "InternalIP":
@@ -39,16 +35,13 @@ def get_instance_id_by_internal_ip(node):
                 return response['Reservations'][0]['Instances'][0]['InstanceId']
     return None
 
-# Функція для отримання деталей інстансу EC2
 def get_instance_details(instance_id):
     instance_details = ec2_client.describe_instances(InstanceIds=[instance_id])
     instance_type = instance_details['Reservations'][0]['Instances'][0]['InstanceType']
     price = get_instance_price(instance_type)
     return instance_type, price
 
-# Функція для отримання ціни інстансу
 def get_instance_price(instance_type):
-    # Виконуємо запит до Pricing API
     pricing_client = boto3.client('pricing', region_name=pricing_region)
     response = pricing_client.get_products(
         ServiceCode='AmazonEC2',
@@ -58,19 +51,48 @@ def get_instance_price(instance_type):
         ]
     )
 
-    # Перевіряємо наявність результатів
     if 'PriceList' in response and response['PriceList']:
         for price_item in response['PriceList']:
-            price_item_dict = json.loads(price_item)  # Перетворюємо рядок JSON в словник
+            price_item_dict = json.loads(price_item)
             if 'terms' in price_item_dict:
                 price_dimensions = price_item_dict['terms']['OnDemand']
                 for key in price_dimensions:
                     price = price_dimensions[key]['priceDimensions']
                     for price_key in price:
-                        return price[price_key]['pricePerUnit']['USD']
-    return "Unknown"
+                        return float(price[price_key]['pricePerUnit']['USD'])
+    return 0.0
 
-# Функція для отримання утилізації CPU та пам'яті
+def get_pod_cpu_usage(node):
+    pods = v1.list_pod_for_all_namespaces(field_selector=f'spec.nodeName={node.metadata.name}').items
+    total_cpu_usage = 0
+
+    for pod in pods:
+        if pod.spec.containers:
+            for container in pod.spec.containers:
+                resources = container.resources
+                if resources.requests and 'cpu' in resources.requests:
+                    cpu_request = resources.requests['cpu']
+                    total_cpu_usage += int(cpu_request.replace('m', '')) / 1000 if 'm' in cpu_request else int(cpu_request)
+
+    return total_cpu_usage  # Повертаємо в vCPUs
+
+def get_pod_memory_usage(node):
+    pods = v1.list_pod_for_all_namespaces(field_selector=f'spec.nodeName={node.metadata.name}').items
+    total_memory_usage = 0
+
+    for pod in pods:
+        if pod.spec.containers:
+            for container in pod.spec.containers:
+                resources = container.resources
+                if resources.requests and 'memory' in resources.requests:
+                    memory_request = resources.requests['memory']
+                    if memory_request.endswith('Mi'):
+                        total_memory_usage += int(memory_request[:-2]) / 1024  # В GiB
+                    elif memory_request.endswith('Gi'):
+                        total_memory_usage += int(memory_request[:-2])  # В GiB
+
+    return total_memory_usage  # Повертаємо в GiB
+
 def get_node_utilization(node):
     cpu_allocatable_str = node.status.allocatable['cpu']
     cpu_allocatable = int(cpu_allocatable_str.replace('m', '')) / 1000 if 'm' in cpu_allocatable_str else int(cpu_allocatable_str)
@@ -80,64 +102,85 @@ def get_node_utilization(node):
     cpu_capacity = int(cpu_capacity_str.replace('m', '')) / 1000 if 'm' in cpu_capacity_str else int(cpu_capacity_str)
     memory_capacity = int(node.status.capacity['memory'].replace('Ki', '')) / (1024 * 1024)
 
-    cpu_utilization = (cpu_allocatable / cpu_capacity) * 100
-    memory_utilization = (memory_allocatable / memory_capacity) * 100
-    return cpu_utilization, memory_utilization
+    # Отримуємо фактичну зайнятість
+    used_cpu = get_pod_cpu_usage(node)
+    used_memory = get_pod_memory_usage(node)
 
-# Функція для виведення прогрес-бару у стилі htop
-def display_htop_style(cpu_utilization, memory_utilization):
-    # Визначення кольорів для прогрес-барів
-    cpu_color = Fore.GREEN if cpu_utilization >= 90 else Fore.YELLOW if cpu_utilization >= 30 else Fore.RED
-    memory_color = Fore.GREEN if memory_utilization >= 90 else Fore.YELLOW if memory_utilization >= 30 else Fore.RED
+    cpu_utilization = (used_cpu / cpu_capacity) * 100 if cpu_capacity > 0 else 0
+    memory_utilization = (used_memory / memory_capacity) * 100 if memory_capacity > 0 else 0
+    return cpu_utilization, memory_utilization, cpu_capacity, memory_capacity
 
-    # Створення прогрес-барів
-    cpu_bar = cpu_color + '█' * int(cpu_utilization // 5) + ' ' * (20 - int(cpu_utilization // 5)) + Fore.RESET
-    memory_bar = memory_color + '█' * int(memory_utilization // 5) + ' ' * (20 - int(memory_utilization // 5)) + Fore.RESET
+def display_progress_bar(value):
+    bar_length = 20  # Довжина прогрес-бару
+    filled_length = int(bar_length * (value / 100))
+    bar = '█' * filled_length + ' ' * (bar_length - filled_length)
 
-    # Виведення прогрес-барів
-    print(f"CPU: [{cpu_bar}] {cpu_utilization:.2f}%")
-    print(f"Memory: [{memory_bar}] {memory_utilization:.2f}%")
+    color = Fore.RED if value < 30 else Fore.YELLOW if value < 90 else Fore.GREEN
+    return f"{color}[{bar}] {value:.2f}%{Fore.RESET}"
 
-# Основна функція для аналізу нод
 def analyze_nodes():
     while True:
         total_cpu_utilization = 0
         total_memory_utilization = 0
+        total_cpu_capacity = 0
+        total_memory_capacity = 0
+        total_cost = 0.0
         node_count = 0
+        node_data = []
 
         for node in nodes:
             instance_id = get_instance_id(node)
             if instance_id:
                 instance_type, price = get_instance_details(instance_id)
-                cpu_utilization, memory_utilization = get_node_utilization(node)
+                cpu_utilization, memory_utilization, cpu_capacity, memory_capacity = get_node_utilization(node)
 
-                print(f"Instance ID: {node.metadata.name}")
-                print(f"Instance Type: {instance_type}")
-                print(f"Node Pricing: ${price}/hour")  # Ціна, отримана з функції
+                node_data.append({
+                    "name": node.metadata.name,
+                    "instance_type": instance_type,
+                    "price": price,
+                    "cpu_capacity": cpu_capacity,
+                    "memory_capacity": memory_capacity,
+                    "cpu_utilization": cpu_utilization,
+                    "memory_utilization": memory_utilization
+                })
 
-                # Відображаємо прогрес-бари для утилізації CPU та пам'яті
-                display_htop_style(cpu_utilization, memory_utilization)
-
-                # Накопичуємо загальні значення
                 total_cpu_utilization += cpu_utilization
                 total_memory_utilization += memory_utilization
+                total_cpu_capacity += cpu_capacity
+                total_memory_capacity += memory_capacity
+                total_cost += price
                 node_count += 1
             else:
                 print(f"Error: Could not retrieve instance ID for node {node.metadata.name}")
 
-        # Обчислюємо загальну утилізацію
         if node_count > 0:
             avg_cpu_utilization = total_cpu_utilization / node_count
             avg_memory_utilization = total_memory_utilization / node_count
+
+            print("\n" + "-" * 130)
+            print(
+                f"{'Node Name':<30} | {'Instance Type':<20} | {'Node Pricing':<15} | {'CPU Capacity':<15} | {'Memory Capacity':<15} | {'CPU Utilization':<20} | {'Memory Utilization':<20}")
+            print("-" * 130)
+            for data in node_data:
+                cpu_bar = display_progress_bar(data["cpu_utilization"])
+                memory_bar = display_progress_bar(data["memory_utilization"])
+
+                print(
+                    f"{data['name']:<30} | {data['instance_type']:<20} | ${data['price']:.4f}/hour     | {data['cpu_capacity']:<15} | {data['memory_capacity']:<15} | {cpu_bar} | {memory_bar}")
+
+            print("-" * 130)
             print(f"\nAverage CPU Utilization for all nodes: {avg_cpu_utilization:.2f}%")
             print(f"Average Memory Utilization for all nodes: {avg_memory_utilization:.2f}%")
+            print(f"Total Nodes: {node_count}")
+            print(f"Total CPU Capacity: {total_cpu_capacity:.2f} vCPUs")
+            print(f"Total Memory Capacity: {total_memory_capacity:.2f} GiB")
+            print(f"Total Cost for all nodes: ${total_cost:.4f}/hour")
         else:
             print("\nNo nodes found for utilization analysis.")
 
         print("\nPress Ctrl+C to quit...")
-        time.sleep(5)  # Затримка перед повторним аналізом
+        time.sleep(5)
 
-# Викликаємо функцію аналізу нод
 try:
     analyze_nodes()
 except KeyboardInterrupt:
